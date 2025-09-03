bc91f54c7ebb5779691a63819a5ee8f5aeda19a1
임지연 <yoyt12@naver.com>
2025-09-03 22:05:17 +0900

    a

--- ch04/tuple/tuple3.py
+++ ch04/tuple/tuple3.py
@@ -1,14 +1,17 @@
 # 형변환
+
 # 리스트나 문자열을 튜플로 변환할 수 있다
 
 # list -> tuple
-t1 = tuple([4, 0, 2])
+t1 = tuple([1, 2, 3])
 print(t1, type(t1))
 
 # string -> tuple
+# 문자열을 한글자씩 잘라서 튜플로 변환
 t2 = tuple('string')
 print(t2, type(t2))
 
+# 반대로 튜플을 리스트로 변환 가능
 # tuple -> list
-tup = (1, 2, 3)
-print(list(tup))
+t = (1, 2, 3)
+print(list(t))
--- ch04/tuple/tuple4.py
+++ ch04/tuple/tuple4.py
@@ -1,16 +1,9 @@
 # 튜플 값 분리하기
-t1 = (4, 5, 6)
-a, b, c = t1
+t1 = (1, 2, 3)
+a, b, c = t1 # 튜플을 변수에 순서대로 나눠 담기
 print(a, b, c)
-
-# 변수 값 교환하기
-a, b = 1, 2
-print(a, b)
-
-b, a = a, b
-print(a, b)
 
 # *rest : 나머지 값을 모아서 리스트에 담기
 values = (1, 2, 3, 4, 5)
-a, b, *rest = values
+a, b, *rest = values # ab에 12를 담고 나머지는 rest에
 print(a, b, rest)
