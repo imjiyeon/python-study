4186aea6ca72e4fca8b6ba605d70dd7a9275046e
imjiyeon <yoyt12@naver.com>
2025-09-08 14:12:18 +0900

    A

--- ch06/func/quiz1.py
+++ ch06/func/quiz1.py
@@ -1,16 +1,4 @@
-# 두 개의 숫자를 입력받아 더한 값을 반환하는 함수를 작성하고 호출하세요
-def add(a, b):
-    return a + b
-
-result = add(3, 7)
-print(result)   # 10
-
-# 이름과 인사말을 받아 '이름 + 인사말' 형태의 문자열을 출력하는 함수를 작성하세요
-def greet(name, message):
-    print(name, message) 
-
-greet("둘리", "안녕")  # 둘리 안녕
-
+# [입력값x 반환값x]
 # 아래와 같이 오늘의 인사말을 출력하는 함수를 작성하세요
 # "안녕하세요, 오늘도 좋은 하루 되세요!"
 def hello():
@@ -18,6 +6,14 @@ def hello():
 
 hello()
 
+# 오늘의 날씨를 출력하는 함수를 작성하세요
+# 예) "오늘 날씨는 맑음입니다."
+def weather():
+    print("오늘 날씨는 맑음입니다.")
+
+weather()   # 오늘 날씨는 맑음입니다.
+
+# [입력값o 반환값o]
 # 두 개의 숫자를 입력받아 곱한 값을 반환하는 함수를 작성하세요
 def multiply(a, b):
     return a * b
@@ -25,16 +21,15 @@ def multiply(a, b):
 result = multiply(4, 6)
 print(result)   # 출력: 24
 
-# 오늘의 날씨를 출력하는 함수를 작성하세요
-# "오늘 날씨는 맑음입니다."
-def weather():
-    print("오늘 날씨는 맑음입니다.")
-
-weather()   # 오늘 날씨는 맑음입니다.
-
+# [입력값o 반환값x]
 # 이름을 입력받아 환영 인사를 출력하는 함수를 작성하세요
 def welcome(name):
     print(name + "님, 환영합니다!")
 
 welcome("또치")   # 또치님, 환영합니다!
 
+# 이름과 인사말을 받아 '이름 + 인사말' 형태의 문자열을 출력하는 함수를 작성하세요
+def greet(name, message):
+    print(name, message) 
+
+greet("둘리", "안녕")  # 둘리 안녕
