1a73e5e6a51c55c997cd3e28176fc49cf33842eb
임지연 <yoyt12@naver.com>
2025-09-11 01:33:10 +0900

    a

---
+++ mini-project/app-1.py
@@ -0,0 +1,63 @@
+orders = []  # 모든 주문을 담는 리스트
+
+def generate_order_no():
+    if  len(orders) == 0:
+        return 1
+    else:
+        # 음수 인덱스: -1은 리스트의 마지막 요소
+        return orders[-1]["order_no"] + 1
+
+# 주문번호는 생성
+# 나머지는 입력받은 그대로
+def create_order(customer, product, count, price):
+    order = {
+        "order_no": generate_order_no(),
+        "customer": customer,
+        "product": product,
+        "count": int(count),
+        "price": int(price)
+    }
+    return order
+
+def list_all():
+    if len(orders) == 0:
+        print("(주문 없음)")
+    else:
+        for o in orders:
+            total = o["count"] * o["price"]
+            print(
+                "주문번호:", o["order_no"],
+                "고객명:", o["customer"],
+                "제품명:", o["product"],
+                "수량:", o["count"],
+                "단가:", o["price"],
+                "총액:", total
+            )
+
+while True:
+    print("\n1. 상품 주문하기")
+    print("2. 전체 주문 이력 보기")
+    print("3. 고객별 주문 통계")
+    print("4. 끝내기")
+    choice = input("옵션을 선택하세요: ")
+
+    if choice == "1":
+        customer = input("고객명: ")
+        product  = input("제품명: ")
+        count    = input("제품수량: ")
+        price    = input("제품가격: ")
+        order = create_order(customer, product, count, price)
+        orders.append(order)
+        print("주문이 완료되었습니다.")
+
+    elif choice == "2":
+        list_all()
+
+    elif choice == "3":
+        # 여기에 코드를 작성해주세요
+        pass
+
+    elif choice == "4":
+        print("프로그램을 종료합니다.")
+        break
+
---
+++ mini-project/app-2.py
@@ -0,0 +1,78 @@
+orders = []  # 모든 주문을 담는 리스트
+
+def generate_order_no():
+    if  len(orders) == 0:
+        return 1
+    else:
+        # 음수 인덱스: -1은 리스트의 마지막 요소
+        return orders[-1]["order_no"] + 1
+
+# 주문번호는 생성
+# 나머지는 입력받은 그대로
+def create_order(customer, product, count, price):
+    order = {
+        "order_no": generate_order_no(),
+        "customer": customer,
+        "product": product,
+        "count": int(count),
+        "price": int(price)
+    }
+    return order
+
+def list_all():
+    if len(orders) == 0:
+        print("(주문 없음)")
+    else:
+        for o in orders:
+            total = o["count"] * o["price"]
+            print(
+                "주문번호:", o["order_no"],
+                "고객명:", o["customer"],
+                "제품명:", o["product"],
+                "수량:", o["count"],
+                "단가:", o["price"],
+                "총액:", total
+            )
+
+def count_by_customer(name):
+    count = 0
+    for o in orders:
+        if o["customer"] == name:
+            count = count + 1
+    return count
+
+def total_price_by_customer(name):
+    total = 0
+    for o in orders:
+        if o["customer"] == name:
+            total = total + (o["count"] * o["price"])
+    return total
+
+while True:
+    print("\n1. 상품 주문하기")
+    print("2. 전체 주문 이력 보기")
+    print("3. 고객별 주문 통계")
+    print("4. 끝내기")
+    choice = input("옵션을 선택하세요: ")
+
+    if choice == "1":
+        customer = input("고객명: ")
+        product  = input("제품명: ")
+        count    = input("제품수량: ")
+        price    = input("제품가격: ")
+        order = create_order(customer, product, count, price)
+        orders.append(order)
+        print("주문이 완료되었습니다.")
+
+    elif choice == "2":
+        list_all()
+
+    elif choice == "3":
+        name = input("고객명: ")
+        print("주문 건수:", count_by_customer(name))
+        print("주문 금액:", total_price_by_customer(name))
+
+    elif choice == "4":
+        print("프로그램을 종료합니다.")
+        break
+
